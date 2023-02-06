d397efeae6aadf364e2245cc7638b4870677819a
Claudia Lertora <claudia.lertora@thoughtworks.com>
2023-02-06 11:17:12 +0100

    Added perimeter calculation

--- src/main/java/oop/Rectangle.java
+++ src/main/java/oop/Rectangle.java
@@ -5,12 +5,24 @@ import java.util.List;
 public class Rectangle {
 
   private int sides = 4;
+  private int base;
+  private int height;
   private List<Integer> angleDegrees = List.of(90,90,90,90);
+
+  public Rectangle(int base, int height) {
+    this.base = base;
+    this.height = height;
+  }
+
   public int numberOfSides() {
     return sides;
   }
 
   public List<Integer> getAngleDegrees() {
     return angleDegrees;
+  }
+
+  public int perimeter() {
+    return 2*base + 2*height;
   }
 }
--- src/test/java/oop/RectangleTest.java
+++ src/test/java/oop/RectangleTest.java
@@ -10,18 +10,27 @@ public class RectangleTest {
 
   @Test
   public void itShouldHaveFourSides() {
-      Rectangle rectangle = new Rectangle();
+      Rectangle rectangle = new Rectangle(80, 20);
       int total = rectangle.numberOfSides();
       assertEquals(4, total);
   }
 
   @Test
   public void itShouldHaveAllRightAngles() {
-    Rectangle rectangle = new Rectangle();
+    Rectangle rectangle = new Rectangle(80, 20);
     List<Integer> actual = rectangle.getAngleDegrees();
     List<Integer> expected = List.of(90,90,90,90);
     assertEquals(expected, actual);
   }
+
+  @Test
+  public void itShouldCalculateItsPerimeter() {
+    Rectangle rectangle = new Rectangle(60, 20);
+    int actual = rectangle.perimeter();
+    assertEquals(160, actual);
+  }
+
+
   
   
 
